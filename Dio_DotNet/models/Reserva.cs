c391118c1548f3d792b97003ff4a8d6ad94dad9d
Francisco do Carmo Amorim <86329191+FCAmorim@users.noreply.github.com>
2024-09-08 22:20:32 -0300

    Add files via upload

---
+++ Dio_DotNet/models/Reserva.cs
@@ -0,0 +1,51 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception ("Número de pessoas excede a capacidade da suite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Suite.Capacidade;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados*Suite.ValorDiaria;
+
+            if (DiasReservados>10)
+            {
+                valor = 0.9m*valor;
+            }
+
+            return valor;
+        }
+    }
+}
